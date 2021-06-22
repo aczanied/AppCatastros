309b9294c48f83863ec1509fec60f6aca0bd8c4a
paoyomira <paoyomira@hotmail.com>
2021-06-21 21:10:36 -0500

    combos y variables esrural esurbano

--- src/app/_models/maestros/generales.ts
+++ src/app/_models/maestros/generales.ts
@@ -20,6 +20,8 @@ export class LocalData {
     ruta: string = '';
     api: boolean = false;
     tablaBase: string = '';
+    esUrbano: boolean = false;
+    esRural: boolean = false;
 }
 
 // Configurar el diccionario, una vez agregados se recomienda no alterar los _id
@@ -30,43 +32,198 @@ export const diccionario: LocalData[] = [
         tablaBase: 'contador_rural',
         api: false,
         ruta: '',
+        esUrbano: true,
+        esRural: false,
     },
+    
     // Seccion de Combos
+    // 1.  Sincronizar Combo Informacion Basica
+     {
+        _id: 'InformacionBasica', // No alterar
+        tablaBase: 'informacion_basica',
+        api: true,
+        ruta: 'catastro-urbano/informacion-basica-predio',
+        esUrbano: true,
+        esRural: false,
+     },
+
+    // 2. Sincronizar Combo Informacion Propietario 1
+    {
+        _id: 'InformacionPropietario', // No alterar
+        tablaBase: 'informacion_propietarios',
+        api: true,
+        ruta: 'catastro-urbano/informacion-propietario',
+        esUrbano: true,
+        esRural: false,
+     },
+
+    // 3. Sincronizar Combo Informacion Ocupante
+    {
+        _id: 'InformacionOcupante', // No alterar
+        tablaBase: 'informacion_ocupantes',
+        api: true,
+        ruta: 'catastro-urbano/informacion-ocupante',
+        esUrbano: true,
+        esRural: false,
+     },
+
+    // 4. Sincronizar Combo Informacion Uso Predio
+    {
+        _id: 'InformacionUsoPredio', // No alterar
+        tablaBase: 'uso_predios',
+        api: true,
+        ruta: 'catastro-urbano/caracteristicas/uso-predio',
+        esUrbano: true,
+        esRural: false,
+     },
+
+    // 5. Sincronizar Combo Copropietarios
+    {
+        _id: 'Copropietario', // No alterar
+        tablaBase: 'copropietarios',
+        api: true,
+        ruta: 'catastro-urbano/caracteristicas/copropietarios',
+        esUrbano: true,
+        esRural: false,
+     },
+
+    // 6. Sincronizar Combo Detalle
+    {
+        _id: 'Detalle', // No alterar
+        tablaBase: 'detalles',
+        api: true,
+        ruta: 'catastro-urbano/construcciones/detalle',
+        esUrbano: true,
+        esRural: false,
+     },
+
+    // 7. Sincronizar Combo Contribuyente Predios
+    {
+        _id: 'ContribuyentePredio', // No alterar
+        tablaBase: 'contribuyente_predios',
+        api: true,
+        ruta: 'catastro-urbano/informacion-contribuyente-predios',
+        esUrbano: true,
+        esRural: false,
+     },
+
+      // 8. Sincronizar Combo Datos Predio
+    {
+        _id: 'DatosPredio', // No alterar
+        tablaBase: 'datos_predios',
+        api: true,
+        ruta: 'catastro-urbano/informacion-datos-predio',
+        esUrbano: true,
+        esRural: false,
+     },
+
+      // 9. Sincronizar Combo Aseo Calles
+    {
+        _id: 'AseoCalle', // No alterar
+        tablaBase: 'aseo_calles',
+        api: true,
+        ruta: 'catastro-urbano/datos-predio/informacion-general/aseo-calles',
+        esUrbano: true,
+        esRural: false,
+     },
+
+    // 10. Sincronizar Combo Tipo Ocupante
+    {
+        _id: 'TipoOcupante', // No alterar
+        tablaBase: 'tipo_ocupantes',
+        api: true,
+        ruta: 'catastro-urbano/construccion/datos-adicionales/tipo-ocupantes',
+        esUrbano: true,
+        esRural: false,
+     },
+
+    // 11. Sincronizar Combo Tipo Viviendas
+    {
+        _id: 'TipoVivienda', // No alterar
+        tablaBase: 'tipo_viviendas',
+        api: true,
+        ruta: 'catastro-urbano/construccion/datos-adicionales/tipo-viviendas',
+        esUrbano: true,
+        esRural: false,
+     },
+
+    // 12. Sincronizar Combo Valor Cultural
+     {
+        _id: 'ValorCultural', // No alterar
+        tablaBase: 'valor_cultural',
+        api: true,
+        ruta: 'catastro-urbano/construccion/datos-adicionales/valor-cultural',
+        esUrbano: true,
+        esRural: false,
+     },
+
+    // 13. Sincronizar Combo Tipo Mejora 
+    {
+        _id: 'TipoMejora', // No alterar
+        tablaBase: 'tipo_mejora_cmb',
+        api: true,
+        ruta: 'catastro-urbano/construccion/mejoras/tipo-mejora',
+        esUrbano: true,
+        esRural: false,
+     },
+
+    // 13. Sincronizar Combo Tipo Mejora 
+    {
+        _id: 'TipoMejora', // No alterar
+        tablaBase: 'tipo_mejoras',
+        api: true,
+        ruta: 'catastro-urbano/construccion/mejoras/tipo-mejora',
+        esUrbano: true,
+        esRural: false,
+     },
+
     {
         _id: 'Entrepisos', // No alterar
         tablaBase: 'entrepisos',
         api: true,
         ruta: 'catastro-urbano/construccion/estructuras/entrepisos',
+        esUrbano: true,
+        esRural: false,
      },
      {
         _id: 'Columnas', // No alterar
         tablaBase: 'columnas',
         api: true,
         ruta: 'catastro-urbano/construccion/estructuras/columnas',
+        esUrbano: true,
+        esRural: false,
      },
      {
         _id: 'Uso Suelo', // No alterar
         tablaBase: 'uso-suelo',
         api: true,
         ruta: 'catastro-urbano/datos-predio/informacion-general/uso-suelo',
+        esUrbano: true,
+        esRural: false,
      },
     {
         _id: 'Cubiertas', // No alterar
         tablaBase: 'cubiertas',
         api: true,
         ruta: 'catastro-urbano/construccion/acabados/cubiertas',
+        esUrbano: true,
+        esRural: false,
      },
      {
         _id: 'Parroquias', // No alterar
         tablaBase: 'parroquias',
         api: true,
         ruta: 'catastro-urbano/parroquias',
+        esUrbano: true,
+        esRural: false,
      },
     {
        _id: 'InformacionPropietario', // No alterar
        tablaBase: 'informacion_propietario',
        api: false,
         ruta: 'catastro-urbano/informacion-propietario',
+        esUrbano: true,
+        esRural: false,
     }];
 
 
