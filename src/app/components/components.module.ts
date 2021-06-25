9095c91d913fa148911ea138dd5da95de86fc2be
Eduardo Acosta <aczanied@gmail.com>
2021-06-24 22:45:34 -0500

    Fix Menu

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
-
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 
 @NgModule({
   declarations: [MenuComponent],
   imports: [
     CommonModule,
+    IonicModule
   ],
   exports: [
     MenuComponent
